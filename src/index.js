7901dacf2a65bb15fb7998a2aa15ed83e9c65375
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2017-06-12 11:58:28 +0200

    use the latest version of file reader

--- src/index.js
+++ src/index.js
@@ -1,26 +1,37 @@
 import program from 'commander';
-import Representation from 'representation';
-import reader from 'representation-tool-file-reader';
+import fs from 'fs';
+import path from 'path';
 import logWith from 'log-with';
+import Representation from 'representation';
+import ConfigReader from 'representation-tool-file-reader';
 import pkg from '../package.json';
 
 const logger = logWith(module);
 
 program
   .version(pkg.version)
-  .option('-c, --config [path]', 'Set config path. defaults to ./config.yml', './config.yml')
-  .option('-d, --debug', 'Log everything')
+  .option('-c, --config [path]', 'Set config path. defaults to ./config.yml')
+  .option('-l, --log', 'Log everything')
   .parse(process.argv);
 
-if (program.debug) {
+if (program.log) {
   logger.level = 'debug';
 }
+const ls = (folder) => {
+  const dir = path.resolve(process.cwd(), folder);
+  logger.debug('Listing files');
+  fs.readdirSync(dir).forEach((file)=> {
+    logger.debug(`${folder}/${file}`);
+  });
+};
+
 logger.debug('Starting to build');
 const env = process.env;
-const config = reader(program.config, env);
+const config = ConfigReader.read(program.config, env);
 const representation = new Representation(config);
 logger.debug('Building');
 representation.build()
   .then(() => {
+    ls(config.folder);
     logger.debug('Done');
   });
