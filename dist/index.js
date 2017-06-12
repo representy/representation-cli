bf63c5d26e1ee403ff6d6b1b64db39a9b3ed052a
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2017-06-12 12:05:55 +0200

    update dist

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,18 @@ var _commander = require('commander');
 
 var _commander2 = _interopRequireDefault(_commander);
 
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _logWith = require('log-with');
+
+var _logWith2 = _interopRequireDefault(_logWith);
+
 var _representation = require('representation');
 
 var _representation2 = _interopRequireDefault(_representation);
@@ -11,10 +23,6 @@ var _representation2 = _interopRequireDefault(_representation);
 var _representationToolFileReader = require('representation-tool-file-reader');
 
 var _representationToolFileReader2 = _interopRequireDefault(_representationToolFileReader);
-
-var _logWith = require('log-with');
-
-var _logWith2 = _interopRequireDefault(_logWith);
 
 var _package = require('../package.json');
 
@@ -24,16 +32,25 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 const logger = (0, _logWith2.default)(module);
 
-_commander2.default.version(_package2.default.version).option('-c, --config [path]', 'Set config path. defaults to ./config.yml', './config.yml').option('-d, --debug', 'Log everything').parse(process.argv);
+_commander2.default.version(_package2.default.version).option('-c, --config [path]', 'Set config path. defaults to ./config.yml').option('-l, --log', 'Log everything').parse(process.argv);
 
-if (_commander2.default.debug) {
+if (_commander2.default.log) {
   logger.level = 'debug';
 }
+const ls = folder => {
+  const dir = _path2.default.resolve(process.cwd(), folder);
+  logger.debug('Listing files');
+  _fs2.default.readdirSync(dir).forEach(file => {
+    logger.debug(`${folder}/${file}`);
+  });
+};
+
 logger.debug('Starting to build');
 const env = process.env;
-const config = (0, _representationToolFileReader2.default)(_commander2.default.config, env);
+const config = _representationToolFileReader2.default.read(_commander2.default.config, env);
 const representation = new _representation2.default(config);
 logger.debug('Building');
 representation.build().then(() => {
+  ls(config.folder);
   logger.debug('Done');
 });
